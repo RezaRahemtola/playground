7914f9ae1e7779c16435e882ed78ef90217b688c
Reza Rahemtola <reza.rahemtola@epitech.eu>
2022-04-18 14:46:56 +0200

    [ADD] First component

--- react/pool2022-day05/artists-book/src/app/App.tsx
+++ react/pool2022-day05/artists-book/src/app/App.tsx
@@ -1,13 +1,8 @@
-import { Center, Spinner, Text, VStack } from '@chakra-ui/react';
+import Home from 'pages/Home';
 
 const App = (): JSX.Element => (
 	<>
-		<Center mt="160px">
-			<VStack spacing="32px">
-				<Text>It's time for you to start your frontend development</Text>
-				<Spinner w="132px" h="132px" />
-			</VStack>
-		</Center>
+		<Home />
 	</>
 );
 
---
+++ react/pool2022-day05/artists-book/src/pages/Home.tsx
@@ -0,0 +1,11 @@
+import { Center, Heading } from '@chakra-ui/react';
+
+function Home(): JSX.Element {
+	return (
+		<Center>
+			<Heading color="tomato">Artists Book</Heading>
+		</Center>
+	);
+}
+
+export default Home;
