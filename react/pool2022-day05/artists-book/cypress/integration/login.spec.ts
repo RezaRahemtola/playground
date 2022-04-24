906dc4917a96c25c61e70aa2535d6599c7c1dc51
Reza Rahemtola <reza.rahemtola@epitech.eu>
2022-04-24 16:04:55 +0200

    [FIX] Cypress tests optimized

--- react/pool2022-day05/artists-book/cypress/integration/login.spec.ts
+++ react/pool2022-day05/artists-book/cypress/integration/login.spec.ts
@@ -3,13 +3,11 @@ describe('Buttons and links', function () {
     cy.visit('http://localhost:3000/login');
     cy.get('#login')
   });
+  it('Check number of inputs', function () {
+    cy.get('input').should('have.length', 2);
+  });
   it('Check Register button', function () {
-    cy.visit('http://localhost:3000/login');
     cy.get('#register').click();
     cy.url().should('include', '/register');
-  });
-  it('Check number of inputs', function () {
-    cy.visit('http://localhost:3000/login');
-    cy.get('input').should('have.length', 2);
   });
 });
--- react/pool2022-day05/artists-book/cypress/integration/register.spec.ts
+++ react/pool2022-day05/artists-book/cypress/integration/register.spec.ts
@@ -3,13 +3,11 @@ describe('Buttons and links', function () {
     cy.visit('http://localhost:3000/register');
     cy.get('#register')
   });
+  it('Check number of inputs', function () {
+    cy.get('input').should('have.length', 3);
+  });
   it('Check Login button', function () {
-    cy.visit('http://localhost:3000/register');
     cy.get('#login').click();
     cy.url().should('include', '/login');
-  });
-  it('Check number of inputs', function () {
-    cy.visit('http://localhost:3000/register');
-    cy.get('input').should('have.length', 3);
   });
 });
