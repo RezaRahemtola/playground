8ce02bd6933c732c73b88901daaa8266bf22314b
Reza Rahemtola <reza.rahemtola@epitech.eu>
2022-05-12 21:39:14 +0200

    [ADD] Base file upload with aleph app started

---
+++ aleph/upload-file-app/src/App.tsx
@@ -0,0 +1,38 @@
+import {useEffect} from 'react';
+import {DEFAULT_API_V2} from "aleph-sdk-ts/global";
+import {store} from "aleph-sdk-ts";
+import {ethereum} from "aleph-sdk-ts/accounts";
+import {ItemType} from "aleph-sdk-ts/messages/message";
+
+function App() {
+  useEffect(() => {
+    (async () => {
+      const {account} = ethereum.NewAccount();
+      console.log(account.address);
+
+      const file = new File(
+          ["This is just a test v2."],
+          "test.txt",
+          {
+            type: "text/plain"
+          }
+      );
+
+      const confirmation = await store.Publish({
+        channel: "TEST",
+        account: account,
+        fileObject: file,
+        storageEngine: ItemType.storage,
+        APIServer: DEFAULT_API_V2,
+      });
+
+      console.log(confirmation)
+    })();
+  }, []);
+  return (
+      <div>
+    </div>
+  );
+}
+
+export default App;
