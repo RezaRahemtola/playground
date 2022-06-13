b07456cd3184223b82c8c181119c744c5741a801
Reza Rahemtola <reza.rahemtola@epitech.eu>
2022-06-13 13:18:44 +0200

    feat: Multi argument calculator

--- GithubWorkshop/calculator/src/main.c
+++ GithubWorkshop/calculator/src/main.c
@@ -12,9 +12,8 @@ int main(int argc, char *const argv[])
 {
     int result = 0;
 
-    if (argc != 3)
-        return 84;
-    result = atoi(argv[0]) + atoi(argv[1]);
+    for (int i = 1; i < argc; i++)
+        result += atoi(argv[i]);
     printf("Result is %d\n", result);
     return 0;
 }
