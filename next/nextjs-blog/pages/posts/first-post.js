44bd3499862ad4f08e6ec829f961e73570390760
Reza Rahemtola <reza.rahemtola@epitech.eu>
2022-06-20 23:58:42 +0200

    [ADD] Assets, metadata and CSS for Next tutorial

--- next/nextjs-blog/pages/posts/first-post.js
+++ next/nextjs-blog/pages/posts/first-post.js
@@ -1,14 +1,22 @@
+import Head from 'next/head';
 import Link from 'next/link';
+import Layout from '../../components/layout';
 
 export default function FirstPost() {
   return (
     <>
-      <h1>First Post</h1>
-      <h2>
-        <Link href="/">
-          <a>Back to home</a>
-        </Link>
-      </h2>
+		<Layout>
+			<Head>
+				<title>First Post</title>
+			</Head>
+			<h1>First Post</h1>
+			<h2>
+				<Link href="/">
+					<a>Back to home</a>
+				</Link>
+			</h2>
+		</Layout>
+
     </>
   );
 }
