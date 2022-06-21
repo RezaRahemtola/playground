b3c4a3b713cdde8f1b355dedf17a600f8c6ee4eb
Reza Rahemtola <reza.rahemtola@epitech.eu>
2022-06-21 15:28:41 +0200

    [ADD] Dynamic routes Next tutorial

--- next/nextjs-blog/components/layout.js
+++ next/nextjs-blog/components/layout.js
@@ -44,7 +44,7 @@ export default function Layout({ children, home }) {
               <a>
                 <Image
                   priority
-                  src="/images/profile.jpg"
+                  src="/images/profile.png"
                   className={utilStyles.borderCircle}
                   height={108}
                   width={108}
--- next/nextjs-blog/pages/index.js
+++ next/nextjs-blog/pages/index.js
@@ -2,6 +2,8 @@ import Head from 'next/head';
 import Layout, { siteTitle } from '../components/layout';
 import utilStyles from '../styles/utils.module.css';
 import { getSortedPostsData } from '../lib/posts';
+import Link from 'next/link';
+import Date from '../components/date';
 
 export async function getStaticProps() {
 	const allPostsData = getSortedPostsData();
@@ -30,12 +32,14 @@ export default function Home({ allPostsData }) {
         <ul className={utilStyles.list}>
           {allPostsData.map(({ id, date, title }) => (
             <li className={utilStyles.listItem} key={id}>
-              {title}
-              <br />
-              {id}
-              <br />
-              {date}
-            </li>
+			  <Link href={`/posts/${id}`}>
+			    <a>{title}</a>
+			  </Link>
+			  <br />
+			  <small className={utilStyles.lightText}>
+			    <Date dateString={date} />
+			  </small>
+		  </li>
           ))}
         </ul>
       </section>
