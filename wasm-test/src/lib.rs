ce33982375e73b62bf6b544cf02539c2a88b6e1e
Reza Rahemtola <reza.rahemtola@epitech.eu>
2022-08-18 00:16:58 +0200

    feat: advanced wasm-rust

--- wasm-test/src/lib.rs
+++ wasm-test/src/lib.rs
@@ -9,11 +9,18 @@ use wasm_bindgen::prelude::*;
 static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
 
 #[wasm_bindgen]
-extern {
-    fn alert(s: &str);
+pub fn greet(name: &str) -> String {
+    let val = format!("Hello {}", name);
+    return val;
 }
 
 #[wasm_bindgen]
-pub fn greet() {
-    alert("Hello, wasm-test!");
+pub fn fib (n: i32) -> i32 {
+    if n <= 0 {
+        return 0;
+    }
+    if n== 1{
+        return 1;
+    }
+    return fib (n-1)  + fib(n-2);
 }
