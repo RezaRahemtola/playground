a5650c66fa83de13b3fe1995dc0404d1d84a3d1d
Reza Rahemtola <reza.rahemtola@epitech.eu>
2022-09-24 20:13:02 +0200

    feat: Completed Go workshop Scaleway

--- go/meetup-scaleway/domain/robot.go
+++ go/meetup-scaleway/domain/robot.go
@@ -5,16 +5,16 @@ import (
 )
 
 type Surface struct {
-	Xmax uint
-	Ymax uint
-	Xmin uint
-	Ymin uint
+	Xmax int
+	Ymax int
+	Xmin int
+	Ymin int
 }
 
 type Robot struct {
-	Name string
-	X uint 
-	Y uint 
+	Name      string
+	X         int
+	Y         int
 	Direction uint
 }
 
@@ -22,10 +22,50 @@ func (r Robot) Hello() string {
 	return fmt.Sprintf("Hello, %s", r.Name)
 }
 
-func (r Robot) turnLeft() { 
+func HandleMovements(r *Robot, s Surface, input string) {
+	for _, char := range input {
+		switch char {
+		case 'F':
+			move(r, s, 1)
+		case 'B':
+			move(r, s, -1)
+		case 'L':
+			turnLeft(r)
+		case 'R':
+			turnRight(r)
+		}
+	}
+}
+
+func move(r *Robot, s Surface, nb int) {
+	switch r.Direction {
+	case 0:
+		// Heading north
+		if (nb == 1 && s.Ymax >= r.Y+1) || (nb == -1 && s.Ymin <= r.Y-1) {
+			r.Y += nb
+		}
+	case 1:
+		// Heading east
+		if (nb == 1 && s.Xmax >= r.X+1) || (nb == -1 && s.Xmin <= r.X-1) {
+			r.X += nb
+		}
+	case 2:
+		// Heading south
+		if (nb == 1 && s.Ymin <= r.Y-1) || (nb == -1 && s.Ymax >= r.Y+1) {
+			r.Y -= nb
+		}
+	case 3:
+		// Heading west
+		if (nb == -1 && s.Xmin <= r.X-1) || (nb == -1 && s.Xmax >= r.X+1) {
+			r.X -= nb
+		}
+	}
+}
+
+func turnLeft(r *Robot) {
 	r.Direction = (r.Direction + 3) % 4
 }
 
-func (r Robot) turnRight() { 
+func turnRight(r *Robot) {
 	r.Direction = (r.Direction + 1) % 4
 }
--- go/meetup-scaleway/domain/robot_test.go
+++ go/meetup-scaleway/domain/robot_test.go
@@ -2,104 +2,107 @@ package domain
 
 import (
 	"testing"
+
 	"github.com/stretchr/testify/require"
 )
 
 func TestRobotString(t *testing.T) {
 	testCases := []struct {
-		x, y, direction, xMin, yMin, xMax, yMax uint
-		name, commands string
-		expectedRobot Robot
-	} {
+		x, y, xMin, yMin, xMax, yMax int
+		direction                    uint
+		name, commands               string
+		expectedRobot                Robot
+	}{
 		{
-			name: "Simple forward",
-			x: 0,
-			y: 0,
-			direction: 0,
-			xMin: 0,
-			yMin: 0,
-			xMax: 100,
-			yMax: 100,
-			commands: "F",
-			expectedRobot: Robot{ "robot", 0, 1, 0},
+			name:          "Simple forward",
+			x:             0,
+			y:             0,
+			direction:     0,
+			xMin:          0,
+			yMin:          0,
+			xMax:          100,
+			yMax:          100,
+			commands:      "F",
+			expectedRobot: Robot{"robot", 0, 1, 0},
 		},
 		{
-			name: "Simple backward",
-			x: 0,
-			y: 1,
-			direction: 0,
-			xMin: 0,
-			yMin: 0,
-			xMax: 100,
-			yMax: 100,
-			commands: "B",
-			expectedRobot: Robot{ "robot", 0, 0, 0},
+			name:          "Simple backward",
+			x:             0,
+			y:             1,
+			direction:     0,
+			xMin:          0,
+			yMin:          0,
+			xMax:          100,
+			yMax:          100,
+			commands:      "B",
+			expectedRobot: Robot{"robot", 0, 0, 0},
 		},
 		{
-			name: "Fail backward",
-			x: 0,
-			y: 0,
-			direction: 0,
-			xMin: 0,
-			yMin: 0,
-			xMax: 100,
-			yMax: 100,
-			commands: "B",
-			expectedRobot: Robot{ "robot", 0, 0, 0},
+			name:          "Fail backward",
+			x:             0,
+			y:             0,
+			direction:     0,
+			xMin:          0,
+			yMin:          0,
+			xMax:          100,
+			yMax:          100,
+			commands:      "B",
+			expectedRobot: Robot{"robot", 0, 0, 0},
 		},
 		{
-			name: "Simple left",
-			x: 0,
-			y: 0,
-			direction: 0,
-			xMin: 0,
-			yMin: 0,
-			xMax: 100,
-			yMax: 100,
-			commands: "L",
-			expectedRobot: Robot{ "robot", 0, 0, 3},
+			name:          "Simple left",
+			x:             0,
+			y:             0,
+			direction:     0,
+			xMin:          0,
+			yMin:          0,
+			xMax:          100,
+			yMax:          100,
+			commands:      "L",
+			expectedRobot: Robot{"robot", 0, 0, 3},
 		},
 		{
-			name: "Simple right",
-			x: 0,
-			y: 0,
-			direction: 0,
-			xMin: 0,
-			yMin: 0,
-			xMax: 100,
-			yMax: 100,
-			commands: "R",
-			expectedRobot: Robot{ "robot", 0, 0, 1},
+			name:          "Simple right",
+			x:             0,
+			y:             0,
+			direction:     0,
+			xMin:          0,
+			yMin:          0,
+			xMax:          100,
+			yMax:          100,
+			commands:      "R",
+			expectedRobot: Robot{"robot", 0, 0, 1},
 		},
 		{
-			name: "Simple right + forward",
-			x: 0,
-			y: 0,
-			direction: 0,
-			xMin: 0,
-			yMin: 0,
-			xMax: 100,
-			yMax: 100,
-			commands: "RF",
-			expectedRobot: Robot{ "robot", 1, 0, 1},
+			name:          "Simple right + forward",
+			x:             0,
+			y:             0,
+			direction:     0,
+			xMin:          0,
+			yMin:          0,
+			xMax:          100,
+			yMax:          100,
+			commands:      "RF",
+			expectedRobot: Robot{"robot", 1, 0, 1},
 		},
 		{
-			name: "Simple left + backward",
-			x: 50,
-			y: 0,
-			direction: 0,
-			xMin: 0,
-			yMin: 0,
-			xMax: 100,
-			yMax: 100,
-			commands: "LB",
-			expectedRobot: Robot{ "robot", 3, 0, 51},
+			name:          "Simple left + backward",
+			x:             50,
+			y:             0,
+			direction:     0,
+			xMin:          0,
+			yMin:          0,
+			xMax:          100,
+			yMax:          100,
+			commands:      "LB",
+			expectedRobot: Robot{"robot", 51, 0, 3},
 		},
 	}
 	for _, c := range testCases {
 		t.Run(c.name, func(t *testing.T) {
-			r := Robot { Name: "robot", X: c.x, Y: c.y, Direction: c.direction }
-			//s := Surface { Xmin: c.xMin, Ymin: c.yMin, Xmax: c.xMax, Ymax: c.yMax }
+			r := Robot{Name: "robot", X: c.x, Y: c.y, Direction: c.direction}
+			s := Surface{Xmin: c.xMin, Ymin: c.yMin, Xmax: c.xMax, Ymax: c.yMax}
+			HandleMovements(&r, s, c.commands)
 			require.Equal(t, c.expectedRobot, r)
 		})
 	}
