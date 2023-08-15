3db0cd66f11b17ee024ab0aff9873b963f146f72
Reza Rahemtola <reza.rahemtola@epitech.eu>
2023-08-15 19:12:00 +0200

    feat(rust/book): minigrep basics

--- languages/rust/book/minigrep/src/main.rs
+++ languages/rust/book/minigrep/src/main.rs
@@ -1,3 +1,21 @@
+use std::env;
+use std::process;
+
+use minigrep::Config;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+
+    let config = Config::build(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments: {err}");
+        process::exit(1);
+    });
+
+    println!("Searching for {}", config.query);
+    println!("In file {}", config.file_path);
+
+    if let Err(e) = minigrep::run(config) {
+        println!("Application error: {e}");
+        process::exit(1);
+    }
 }
